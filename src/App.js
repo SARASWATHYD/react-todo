1ffeb2b1abb0c8c99da3d7ae367f1c4b79caaae6
SARASWATHYD <saraswathy.moorthy@anywhere.co>
2019-11-25 10:01:20 +0530

    started day with previous changes

--- src/App.js
+++ src/App.js
@@ -9,55 +9,55 @@ import TodoItem from './components/TodoItem';
 import todoData from './todoData';
 
 
-// class App extends React.Component {
-//   constructor(){
-//   super()
-//     this.state ={
-//       todos : todoData
-//     }
-  
-//   }
- 
-//   render(){
-//   const todoItems = this.state.todos.map(item => <TodoItem key ={item.id} item = {item}/>)
-//   return (
-//     <div className="App">
-//       <header className="App-header">
-//         <div className= "todo-list">
-//      {todoItems}
-//      <button onClick = {handleClick}>  create</button>
-//       </div>
-//       {/* <Headers/>
-//       <MainContent/>
-//       <Footer/> */}
-//       </header>
-//     </div>
-//   );
-// }
-// }
-
-class App extends React.Component{
+class App extends React.Component {
   constructor(){
-    super()
-    this.state = {
-      count : 0
-    } 
-    this.handleClick = this.handleClick.bind(this)
-  }
-    handleClick(){
-      this.setState(prevState => {
-        return {count : prevState.count+1}
-      })
+  super()
+    this.state ={
+      todos : todoData
     }
-  render(){
-    return (
-      <div>
-        <h1>{this.state.count } </h1>
-        <button onClick = {this.handleClick}> click me</button>
-      </div>
-    )
+  
   }
+ 
+  render(){
+  const todoItems = this.state.todos.map(item => <TodoItem key ={item.id} item = {item}/>)
+  return (
+    <div className="App">
+      <header className="App-header">
+        <div className= "todo-list">
+     {todoItems}
+     <button onClick = {handleClick}>  create</button>
+      </div>
+      {/* <Headers/>
+      <MainContent/>
+      <Footer/> */}
+      </header>
+    </div>
+  );
 }
+}
+
+// class App extends React.Component{
+//   constructor(){
+//     super()
+//     this.state = {
+//       count : 0
+//     } 
+//     this.handleClick = this.handleClick.bind(this)
+//   }
+//     handleClick(){
+//       this.setState(prevState => {
+//         return {count : prevState.count+1}
+//       })
+//     }
+//   render(){
+//     return (
+//       <div>
+//         <h1>{this.state.count } </h1>
+//         <button onClick = {this.handleClick}> click me</button>
+//       </div>
+//     )
+//   }
+// }
 
 
 
